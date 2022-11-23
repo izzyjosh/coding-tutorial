92c7524068a1bbf8d0d2465e8aee54334719f925
izzyjosh <calebayuba222@gmail.com>
2022-11-23 18:52:28 +0100

    added a file

---
+++ advanced.py
@@ -0,0 +1,42 @@
+#string = ["a",  "b",  "c"]
+#print("".join(string))
+
+
+#out = ""
+#for i in string:
+#	out += i
+#print(out)
+
+
+#name = "gabi"
+#age = 10
+
+
+#print("{1} {0}".format(name,  age))
+# ASCII ANSI
+
+#print(ord("a"))
+#print(ord("%"))
+#print("\U0001f634")
+
+#print(repr("\U0001f634"))
+
+#emoji = "😁"
+#emoji2 = "😎😍"
+#print("\U0001f60e\U0001f60d")
+#print(emoji2)
+#print(f"{emoji2!a}")
+#print(emoji)
+#print(f"{emoji!a}")
+
+
+#tenary operator
+#num = 5
+
+#if num % 2 == 0:
+#	print("Even")
+#else:
+#	print("Odd")
+#	
+#	
+#print("Even" if num % 2 == 0 else "Odd")
