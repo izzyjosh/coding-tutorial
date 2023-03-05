5675d32796a2a5a116bd5d538d36d30329655838
izzyjosh <calebayuba222@gmail.com>
2023-03-06 00:21:22 +0100

    made some changes

--- file.py
+++ file.py
@@ -9,9 +9,9 @@ file2 = open("/storage/emulated/0/coding tutorial/joshua.txt", "r")
 #file2.close()
 
 # Context Manager!!
-#with open("/storage/emulated/0/coding tutorial/joshua.txt", "a") as file2:
-#	file2.write('\n do u know me\n if yes \n say hi')
-#	
-#	
-#print(r(1,  20))
+with open("/storage/emulated/0/coding tutorial/joshua.txt", "a") as file2:
+	file2.write('\n do u know me\n if yes \n say hi')
+	
+	
+print(r(1,  20))
 print(file2.read())
